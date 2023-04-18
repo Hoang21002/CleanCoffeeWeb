8878c50ab72a53bfb877641ae3d7a2ff1f6c96d6
Hoang21002 <dinhhoang026@gmail.com>
2023-04-19 00:41:03 +0700

    hoan thanh traang xep hang 100%

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,53 @@
+
+// ----------JS CHO BUTTON MENU RESPONSIVE------------------
+
 let navbar = document.querySelector('.header .nav-list');
 
 document.querySelector('.menu-bnt').onclick = () => {
     navbar.classList.toggle('active');
 };
+
+// ----------JS CHO BUTTON MENU RESPONSIVE------------------
+
+
+// ----------JS CHO SLICK SLIDER TRANG XEP HANG------------------
+
+$(document).ready(function () {
+    $('.container-list-xephanh').slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        draggable: false,
+        prevArrow: `<button type='button' class='slick-prev pull-left'><img src="/assets/Icons/ArrowLeft.png" alt=""></button>`,
+        nextArrow: `<button type='button' class='slick-next pull-right'><img src="/assets/Icons/ArrowRight.png" alt=""></button>`,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+});
+
+// ----------JS CHO SLICK SLIDER TRANG XEP HANG------------------
+
 
 
